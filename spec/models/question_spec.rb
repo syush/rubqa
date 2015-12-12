025508bc6f59c7d084c19d1d90c8bf0720c3861c
Sergey Shishlov <shishlov@gmail.com>
2015-12-12 12:38:48 +0300

    Model tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # it 'validates presence of title' do
+  #   question = Question.new(body: '123')
+  #   expect(question).not_to be_valid
+  # end
+
+  it { should validate_presence_of :title }
+
+  it { should validate_presence_of :body }
+
+  it { should have_many :answers }
+
+  it 'should allow 200 or less characters in title' do
+    question = Question.new
+    question.body = "test for short title"
+    question.title = 'X' * 200
+    expect(question).to be_valid
+  end
+
+  it 'should not allow >200 characters iin title' do
+    question = Question.new
+    question.body = "test for long title"
+    question.title = 'X' * 201
+    expect(question).not_to be_valid
+  end
+
+
 end
