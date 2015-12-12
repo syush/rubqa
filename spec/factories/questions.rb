34575ef0d29f70cdb84720f9770f523af7286c83
Sergey Shishlov <shishlov@gmail.com>
2015-12-12 13:53:39 +0300

    Question controller specs

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
+
   factory :question do
-    
+    title "test title"
+    body "test body"
+  end
+
+  factory :invalid_question, class: 'Question' do
+    title nil
+    body nil
   end
 
 end
