248b568183cb0bc1c1285dd5796b14b8076a8ad1
Sergey Shishlov <shishlov@gmail.com>
2015-12-13 08:38:40 +0300

    Answer controller and specs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,23 @@
 class AnswersController < ApplicationController
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to @answer.question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # comment
+  def answer_params
+    params.require(:answer).permit(:body, :question_id)
+  end
+
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,4 +2,56 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
+  before do
+    @question = FactoryGirl.create(:question)
+  end
+
+  describe "GET #new" do
+    before do
+      get :new, question_id: @question.id
+    end
+
+    it 'assigns new Answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders show template' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    context 'valid' do
+      it 'saves new question in DB' do
+        attr = FactoryGirl.attributes_for(:answer)
+        attr[:question_id] = @question.id
+        expect { post :create, question_id: @question.id, answer: attr }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show' do
+        attr = FactoryGirl.attributes_for(:answer)
+        attr[:question_id] = @question.id
+        post :create, question_id: @question.id, answer: attr
+        expect(response).to redirect_to question_path(@question)
+      end
+    end
+
+    context 'invalid' do
+      it 'does not save new answer in DB' do
+        attr = FactoryGirl.attributes_for(:invalid_answer)
+        attr[:question_id] = @question.id
+        answer = FactoryGirl.build(:invalid_answer)
+        expect { post :create, question_id: @question.id, answer: attr }.to_not change(Answer, :count)
+      end
+
+      it 'renders new template' do
+        attr = FactoryGirl.attributes_for(:invalid_answer)
+        attr[:question_id] = @question.id
+        post :create, question_id: @question.id, answer: attr
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+
 end
