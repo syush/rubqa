dd65302179ff4ce9f3360641043090473794f3c6
Sergey Shishlov <shishlov@gmail.com>
2016-01-03 05:09:58 +0300

    authorship check moved from views to helpers

--- app/helpers/questions_controller_helper.rb
+++ app/helpers/questions_controller_helper.rb
@@ -4,4 +4,10 @@ module QuestionsControllerHelper
     answer.question.best_answer_id == answer.id
   end
 
+  def sorted_answers(question)
+    @question.best_answer ?
+        [@question.best_answer] + (@question.answers - [@question.best_answer])
+    : @question.answers
+  end
+
 end
