1ae33a91669462c85e0137ce359c60c554ba8787
Sergey Shishlov <shishlov@gmail.com>
2016-02-17 17:56:07 +0300

    REST API returns  a single question with attachments

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -6,4 +6,9 @@ class Api::V1::QuestionsController < Api::V1::BaseController
     @questions = Question.all
     render json: @questions
   end
+
+  def show
+    render json: Question.find(params[:id])
+  end
+
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -20,7 +20,6 @@ RSpec.describe "Questions API" do
     context 'authorized' do
       let(:access_token) { create(:access_token) }
       let(:user) { create(:user) }
-      let(:other_user) { create(:other_user) }
       let!(:questions) { create_list(:question, 2, user:user) }
       let(:question) { questions.first }
       before { get '/api/v1/questions', format: :json, access_token: access_token.token }
@@ -29,7 +28,7 @@ RSpec.describe "Questions API" do
         expect(response).to be_success
       end
 
-      it 'returnslist of questions' do
+      it 'returns list of questions' do
         expect(response.body).to have_json_size(2).at_path('questions')
       end
 
@@ -60,8 +59,65 @@ RSpec.describe "Questions API" do
         end
       end
 
-
-
     end
   end
+
+  describe "GET /:id" do
+    context "unauthorized" do
+      it 'returns 401 status if access token is invalid' do
+        get "/api/v1/questions/0", format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access token is invalid' do
+        get '/api/v1/questions/0', format: :json, access_token: '1234'
+        expect(response.status).to eq 401
+      end
+
+    end
+
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let(:user) { create(:user) }
+      let(:other_user) { create(:user) }
+      let!(:questions) { create_list(:question, 2, user:user) }
+      let(:question) { questions.first }
+      let!(:attachments) { create_list(:attachment, 3, attachable: question) }
+      let!(:answer) { create(:answer, question:  question, user:other_user) }
+      before { get "/api/v1/questions/#{question.id}", format: :json, access_token: access_token.token }
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr).to_json).at_path("question/#{attr}")
+        end
+      end
+
+      it 'returns list of questions' do
+        expect(response.body).to have_json_size(3).at_path('question/attachments')
+      end
+
+      it "contains file attachments" do
+        expect(response.body).to be_json_eql(attachments[0].id).at_path("question/attachments/2/id")
+        expect(response.body).to be_json_eql(attachments[0].file.url.to_json).at_path("question/attachments/2/file/url")
+      end
+
+      it 'contains answers' do
+        expect(response.body).to have_json_size(1).at_path('question/answers')
+      end
+
+      %w(id body created_at updated_at).each do |attr|
+        it "answer contains #{attr}" do
+          expect(response.body).to be_json_eql(answer.send(attr).to_json).at_path("question/answers/0/#{attr}")
+        end
+      end
+
+    end
+
+  end
+
 end
