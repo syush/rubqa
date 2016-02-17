9b2f72aecb270fa9c73a02458caa1bae1c310d4f
Sergey Shishlov <shishlov@gmail.com>
2016-02-18 01:17:27 +0300

    Mistake fixed in attachable shared examples

--- spec/support/shared/attachable.rb
+++ spec/support/shared/attachable.rb
@@ -1,6 +1,6 @@
 shared_examples_for "Attachable" do
 
-  it { should have_many :attachments }.dependent(:destroy)
+  it { should have_many(:attachments).dependent(:destroy) }
   it { should accept_nested_attributes_for :attachments }
 
 
